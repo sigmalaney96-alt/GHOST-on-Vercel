b1c6c0def72b35bb3244a40e9466d5a25107dfe9
sigmalaney96-alt <sigmalaney96@gmail.com>
2026-09-26 05:45:03 -0400

    Update iframe to load the site directly without UV nested proxying

--- public/example.js
+++ public/example.js
@@ -1,5 +1,5 @@
 // Load Google Sites page as main page on page load
 window.addEventListener("load", function() {
     let url = "https://sites.google.com/view/ghost-ubg";
-    iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+    iframeWindow.src = url;
 });
